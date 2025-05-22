8e7af64e23edf7a3709309b14cd2e6caeb261829
HwiGyoung Lee <ter0000@naver.com>
2025-05-22 15:35:05 +0900

    💄 style(SajuText) : zen-antique 폰트 적용

--- src/components/SajuText.tsx
+++ src/components/SajuText.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+
 interface SajuTextProps {
     size : 'small' | 'medium'
     hanjaText: string;
@@ -7,17 +8,17 @@ interface SajuTextProps {
 export default function SajuText({size ,hanjaText , koreanText}:SajuTextProps) {
   const sizeVariable = {
     small : {
-        hanja : 'text-xs',
-        korean : "text-saju-xs font-main",
+        hanja : 'text-[0.61rem] font-zen-antique whitespace-nowrap',
+        korean : 'text-saju-xs font-main whitespace-nowrap',
     },
     medium :  {
-        hanja : 'text-[0.92rem]',
-        korean : "text-[0.61rem] font-main",
+        hanja : 'text-[0.92rem] font-zen-antique whitespace-nowrap font-bold',
+        korean : "text-[0.61rem] font-main whitespace-nowrap",
     }
   }
   return (
     <div className='flex flex-col text-center leading-[1.1]'>
-      <p className={`${sizeVariable[size]['hanja']}`}>{hanjaText}</p>
+      <p className={`${sizeVariable[size]['hanja']} `}>{hanjaText}</p>
       <p className={`${sizeVariable[size]['korean']}`}>{koreanText}</p>
     </div>
   )
